9b7916462ab878e191f187c135a842d6aa21ffe5
xBlaxk <blaxkmaster@gmail.com>
2023-10-31 12:45:58 -0500

    feat: create function to load library

--- interpreter.c
+++ interpreter.c
@@ -3,3 +3,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+void* current_library = NULL; // Pointer to store the current library handle
+
+void load_library(const char* lib_name) {
+    current_library = dlopen(lib_name, RTLD_LAZY);
+    if (!current_library) {
+        const char* dlsym_error = dlerror();
+        if (strstr(dlsym_error, "file too short") != NULL) {
+            printf("Error: The file '%s' is not a valid shared library.\n", lib_name);
+        } else {
+            printf("Error loading library: %s\n", dlsym_error);
+        }
+        return;
+    }
+}
