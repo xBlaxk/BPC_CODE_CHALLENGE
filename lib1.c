808609aa8614b64e9171d93b9a7faf5ba95c28f8
xBlaxk <blaxkmaster@gmail.com>
2023-10-31 12:51:02 -0500

    feat: add testing shared libraries

---
+++ lib1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void funcA() {
+    printf("Function 'funcA' from lib1.so called!\n");
+}
+
+void funcB() {
+    printf("Function 'funcB' from lib1.so called!\n");
+}
---
+++ lib2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void funcX() {
+    printf("Function 'funcX' from lib2.so called!\n");
+}
+
+void funcY() {
+    printf("Function 'funcY' from lib2.so called!\n");
+}
